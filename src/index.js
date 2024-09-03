df682c36e469dbd59d4150ebd6863ca87919e671
Md Shajib Howlader <120749530+mhshajibat@users.noreply.github.com>
2024-09-03 15:39:37 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
-//
+
 
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
